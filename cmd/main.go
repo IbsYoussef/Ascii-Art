487978fb1f75c8f50fe3e8061fdfacbebd57c1f1
iyoussef <ibraheemyoussefiy@gmail.com>
2026-01-04 21:47:04 +0000

    feat: Integrated core logic into cmd main.go file, program now can render and print ascii art and colored versions of ascii art

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,8 @@ import (
 
 func main() {
 	// Get user input and banner choice
-	input, banner, err := ascii.GetUserInput()
-
-	// If there was an error getting user input, print the error and exit
+	input, banner, colorConfig, err := ascii.GetUserInputWithColor()
+	// Check if error occured when getting user input
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -17,13 +16,16 @@ func main() {
 
 	// Load the ASCII banner from the specified file
 	result, err := ascii.LoadBannerFile(banner)
-
-	// If the banner file is not found, print an error message and exit
+	// Check if error occured with loading specific banner file
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Render the input in banner style as ASCII art
-	ascii.RenderAscii(input, result)
+	// Render the input in banner style as ASCII art, use colour rendering if color flag is present
+	if colorConfig.Enabled {
+		ascii.RenderAsciiWithColor(input, result, colorConfig)
+	} else {
+		ascii.RenderAscii(input, result)
+	}
 }
