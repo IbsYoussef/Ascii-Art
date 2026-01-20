8583f43179e67a4f9919d9dfe7e5c1219c0bd0c5
iyoussef <ibraheemyoussefiy@gmail.com>
2026-01-20 22:43:53 +0000

    test: Removed comment on mocking logic directly

--- test/unit/terminal_test.go
+++ test/unit/terminal_test.go
@@ -60,7 +60,6 @@ func TestFitsInTerminal(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// We can't easily mock GetTerminalWidth, so we test the logic directly
 			fits := tt.contentWidth <= tt.termWidth
 			if fits != tt.expected {
 				t.Errorf("Expected %v, got %v", tt.expected, fits)
